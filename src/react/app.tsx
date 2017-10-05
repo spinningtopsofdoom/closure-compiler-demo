4906a8ad52762e0caa38a0aa9f9e87fd1158e7ac
Peter Schuck <peter@bendyworks.com>
2017-10-05 10:35:40 -0500

    Add base tsx file for todo application

---
+++ src/react/app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+class TodoApp extends React.Component<{}, {}> {
+  public render () {
+    return (
+      <div>Hi</div>
+    );
+  }
+}
+
+function render() {
+  ReactDOM.render(
+    <TodoApp/>,
+    document.getElementById('app')
+  );
+}
