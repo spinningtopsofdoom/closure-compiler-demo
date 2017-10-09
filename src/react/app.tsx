76fca3fdf93605fd81c5cbfeff9e010cfdfd5c4e
Peter Schuck <peter@bendyworks.com>
2017-10-09 09:08:23 -0500

    First pass at working pass of react and closure

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -1,5 +1,13 @@
+/** @const */
+var process: any = {};
+/** @const */
+process.env = {};
+/** @define {string} */
+process.env.NODE_ENV = 'development';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 class TodoApp extends React.Component<{}, {}> {
   public render () {
     return (
